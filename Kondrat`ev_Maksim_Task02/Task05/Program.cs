41f4a466b289ae6eba522d3312875c8c81114de3
Borowik1 <borowik1@yandex.ru>
2019-03-05 22:51:53 +0300

    Task05

---
+++ Kondrat`ev_Maksim_Task02/Task05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Summ=0;
+
+            for(int i = 1; i<= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Summ += i;
+                }
+                if (i % 5 == 0)
+                {
+                    Summ += i;
+                }
+                    
+            }
+
+            Console.Write("Сумма всех натуральных чисел меньших 1000 и кратных трём и пяти: " + Summ);
+            Console.ReadLine();
+        }
+    }
+}
