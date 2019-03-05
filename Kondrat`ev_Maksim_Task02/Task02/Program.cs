d5760c95bcab220ff2e2ba9e6a4024369266dfce
Borowik1 <borowik1@yandex.ru>
2019-03-05 22:34:18 +0300

    Task04

--- Kondrat`ev_Maksim_Task02/Task02/Program.cs
+++ Kondrat`ev_Maksim_Task02/Task02/Program.cs
@@ -10,7 +10,26 @@ namespace Task02
     {
         static void Main(string[] args)
         {
+            int N;
 
+            Console.Write("Введите число строк: ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+               // for (int j = 1; j <= N - i; j++)
+               // {
+               //     Console.Write(" ");
+               // }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Kondrat`ev_Maksim_Task02/Task04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.Write("Введите количество пирамид: ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int l = 1; l <= N; l++)
+            {
+                for (int i = 1; i <= l; i++)
+                {
+                    for (int j = 1; j <= N - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int k = 1; k <= 1 + (i - 1) * 2; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
