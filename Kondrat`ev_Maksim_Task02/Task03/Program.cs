edf467549c4e46c8b72f1e204a6b274d66ea8a38
Borowik1 <borowik1@yandex.ru>
2019-03-05 21:38:13 +0300

    Task03

---
+++ Kondrat`ev_Maksim_Task02/Task03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            
+            Console.Write("Введите число строк пирамиды: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i=1; i <= N; i++)
+            {
+                for (int j=1; j <= N-i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k=1; k <= 1+(i-1)*2; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
